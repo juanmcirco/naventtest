d4d3ba28f0ae1b3db716c79150fd3fb414b856b5
juanmcirco <juanmcirco@gmail.com>
2020-06-30 12:20:53 -0300

    final

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ http://gravitying.com.ar/navent/
 ## Available Scripts
 
 In the project directory, you can run: `yarn` then `yarn start` and enjoy
+
+## Comentarios
+
+Decidí utilizar Context API por la dimension de la evaluacion y me parecio mas simple utilizar context, por otro lado al tener el mock no hice un fake request porque me parecio innesesario. 
+
+En el pricebox utilice "de-DE" para tener un formato similiar al que muestran en el sitio, porque en español los decimales se separan con ","
+
+Nota: Por un cuestion de falta de tiempo y que a su vez no esta requerido en el examen como una necesidad, decidí no realizar test pero de ser necesario podria generarlos.
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,29 @@ import { FiltersContainer } from './FiltersComponents/FiltersContainer'
 import { css } from 'emotion'
 
 const Home = () => {
-    return <div className={homeStyle}>
-        <FiltersContainer/>
-        <Posts />
-    </div>
+    return <>
+        <h2 className={title}>Examen técnico para Navent</h2>
+        <div className={homeStyle}>
+            <FiltersContainer/>
+            <Posts />
+        </div>
+    </>
 }
+
+const title = css({
+    fontFamily: "'Roboto', sans-serif",
+    margin: 'auto',
+    textAlign: 'center',
+    display:'block',
+    paddingTop: '20px'
+})
 
 const homeStyle = css({
     display: 'flex',
     flexDirection: 'row',
     maxWidth: '80vw',
     margin: 'auto',
-    paddingTop: '50px',
+    paddingTop: '20px',
     fontFamily: "'Roboto', sans-serif",
     '@media (max-width: 460px)' :{
         flexDirection: 'column',
